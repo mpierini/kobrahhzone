2743d7de9c847b051176d8436aadfeaf6c8f0248
mpierini <pierini.michele@gmail.com>
2018-02-17 12:29:17 -0800

    committing example config file as placeholder

---
+++ config/config-example.js
@@ -0,0 +1,10 @@
+// example of what my real config file looks like
+var config = {};
+
+config.pinterest = {};
+// generated token for accessing user pinterest info
+config.pinterest.access_token = 'thisismypinteresttoken';
+// port to start up app
+config.port = 0;
+
+module.exports = config;
